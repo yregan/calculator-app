bc2a4c5d64b7284a0f453304a5e4bf34d9044f04
yregan <y.regan@students.amikom.ac.id>
2026-04-09 10:19:06 +0700

    Melengkapi Fungsi Main

--- Calculator.cs
+++ Calculator.cs
@@ -1,7 +1,17 @@
 class Calculator
 {
 	static void Main (string[] args)
-
+	{
+		int a = 10;
+		int b = 6;
+		
+		Console.WriteLine("Hasil Penambahan: {o} + {1} = {2}", a, b, Penambahan(a, b));
+		Console.WriteLine("Hasil Pengurangan: {o} - {1} = {2}", a, b, Pengurangan(a, b));
+		
+		Console.WriteLine("\nTekan Sembarang Key Untuk Keluar");
+		Console.ReadKey();
+	}
+	
 	static int Penambahan(int a, int b,)
 	{
 		return a + b;
